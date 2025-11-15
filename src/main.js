c8fe2faac88571ea0aeed9d8658c1725f7e03a99
yijieng1024 <yijieng1024@gmail.com>
2025-11-16 03:48:31 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://noteflow-production-c250.up.railway.app"
 
 createApp(App).mount('#app');
