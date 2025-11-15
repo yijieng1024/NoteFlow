e156e659b902eb125277d79081f44dc9f48b5518
yijieng1024 <yijieng1024@gmail.com>
2025-11-16 03:18:13 +0800

    update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 // Create a MySQL connection pool
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 // Test connection
